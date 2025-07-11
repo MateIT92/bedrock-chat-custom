c45745b3378c701ed16b6c75514f58ddfc93a8a2
mpoliciolap <159786898+mpoliciolap@users.noreply.github.com>
2025-07-11 11:30:53 +0200

    modifications

--- bin.sh
+++ bin.sh
@@ -48,7 +48,7 @@ ALLOWED_SIGN_UP_EMAIL_DOMAINS=""
 BEDROCK_REGION="us-east-1"
 CDK_JSON_OVERRIDE="{}"
 REPO_URL="https://github.com/MateIT92/bedrock-chat-custom"
-VERSION="v3.2.1"
+VERSION="v3.2.1-custom"
 
 # Parse command-line arguments for customization
 while [[ "$#" -gt 0 ]]; do
